eb413a15751f9241ec76b3d5827c113e452318a5
Rafael Pereira <rafaelps@dcomp.ufs.br>
2019-01-27 00:12:40 -0300

    Run formatter

--- test/medirm_graph_ql_api_web/views/error_view_test.exs
+++ test/medirm_graph_ql_api_web/views/error_view_test.exs
@@ -5,7 +5,9 @@ defmodule MedirmGraphQlApiWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.json" do
-    assert render(MedirmGraphQlApiWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+    assert render(MedirmGraphQlApiWeb.ErrorView, "404.json", []) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500.json" do
