0c1bf4c00f6e349821f966f2e725b9071771e3bb
Rafael Pereira <rafaelps@dcomp.ufs.br>
2019-03-05 11:51:57 -0300

    Add authorization middleware in  query resolver

--- lib/medirm_graph_ql_api_web/Schema.ex
+++ lib/medirm_graph_ql_api_web/Schema.ex
@@ -2,13 +2,15 @@ defmodule MedirmGraphQlApiWeb.Schema do
   use Absinthe.Schema
   
   alias MedirmGraphQlApiWeb.Resolvers
+  alias MedirmGraphQlApiWeb.Schema.Middleware
+
   # import Types
   import_types(MedirmGraphQlApiWeb.Schema.Types)
 
   query do
     @desc "Get a list of all users"
     field :users, list_of(:user_type) do
-      # Resolver
+      middleware(Middleware.Authorize, :any)
       resolve(&Resolvers.UserResolver.users/3)
     end
   end
